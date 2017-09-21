f455f224d63d83333159b9ee8e072761944cb5d9
baotrungtn <baotrungtn@gmail.com>
2017-09-22 01:57:22 +0900

    Fix not update bug?

--- master.py
+++ master.py
@@ -6,21 +6,16 @@ from mit_techreview_spider import MITTechReviewSpider
 from twitter_spider import TwitterSpider
 import logging
 
-hn_spider = HNSpider()
-bb_spider = BloombergSpider()
-mit_spider = MITTechReviewSpider()
-twitter_spider = TwitterSpider()
-
 print("Hello! I am Spider. I will start collecting news...")
 
 while True:
     
     try:
         while True:
-            hn_spider.roam()
-            bb_spider.roam()
-            mit_spider.roam()
-            twitter_spider.roam()
+            HNSpider().roam()
+            BloombergSpider().roam()
+            MITTechReviewSpider().roam()
+            TwitterSpider().roam()
             time.sleep(60)
             print("Reload... " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
